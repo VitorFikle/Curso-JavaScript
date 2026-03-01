cb67eaf3fbbd63e181d69158964c715f96741c88
VitorFikle <vitorSOUND.08@gmail.com>
2026-03-01 14:11:06 -0300

    Add files via upload

---
+++ idade e sexo/script.js
@@ -0,0 +1,48 @@
+window.onLoad = () => {
+
+    const botao = document.querySelector('#submit')
+    const resultado = document.querySelector('#resultado')
+    const idadeInput = document.querySelector('#idade')
+    const sexoInput = document.querySelector('#sexo')
+
+
+    botao.addEventListener('click', function () {
+        let idade = Number(idadeInput.value)
+        let sexo = sexoInput.value
+
+        if (idade > 120 || idade < 0 || isNaN(idade)) {
+            window.alert('Idade inválida. Por favor, insira uma idade entre 0 e 120 anos.')
+            return
+        }
+        else if (sexo !== 'mas' && sexo !== 'fem') {
+            window.alert('Sexo inválido. Por favor, selecione um dos dois sexos disponíveis.')
+            return
+        }
+
+        const img = document.createElement('img')
+
+        if (sexo === 'mas' && idade < 18) { //homem
+            img.src = 'imgs/menino.jpg'
+        }
+        else if (sexo === 'mas' && idade >= 60) {
+            img.src = 'imgs/velho.jpg'
+        }
+        else if (sexo === 'mas' && idade >= 18) {
+            img.src = 'imgs/homem.jpg'
+        }
+
+        else if (sexo === 'fem' && idade >= 60) { //mulher
+            img.src = 'imgs/velha.jpg'
+        }
+        else if (sexo === 'fem' && idade < 18) {
+            img.src = 'imgs/menina.jpg'
+        }
+        else if (sexo === 'fem' && idade >= 18) {
+            img.src = 'imgs/mulher.jpg'
+        }
+
+        resultado.innerHTML = ''
+        resultado.appendChild(img)
+    })
+
+}
